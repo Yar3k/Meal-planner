64e346f37b00081dc5d6e435911c08c6a0b0f3fb
Dmitrijs Sagandikovs <87562103+DmitryS1985@users.noreply.github.com>
2021-12-06 15:55:51 +0200

    First commit

--- src/main/java/main/mealplanner/MealPlannerApplication.java
+++ src/main/java/main/mealplanner/MealPlannerApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MealPlannerApplication {
 
+public class MealPlannerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MealPlannerApplication.class, args);
 	}
