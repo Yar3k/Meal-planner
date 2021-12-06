5fe77ed892ceaf096202cab2f1f622b53cf288a6
Yar3k <djelement567@gmail.com>
2021-12-06 10:07:17 +0200

    Import empty Spring boot application

---
+++ src/main/java/main/mealplanner/MealPlannerApplication.java
@@ -0,0 +1,13 @@
+package main.mealplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealPlannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/main/mealplanner/MealPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package main.mealplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
